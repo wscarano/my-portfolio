b5d7d534b4d45b9405c1a75cbfe2667fcdc61b4f
Wayne Scarano <wscarano@sga.com>
2018-03-25 13:00:34 -0400

    Add SNS notification and Try/Except/Raise

--- upload-demo-lambda.py
+++ upload-demo-lambda.py
@@ -5,25 +5,35 @@ import mimetypes
 
 def lambda_handler(event, context):
     s3 = boto3.resource('s3')
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:743837219496:deployDemoTopic')
 
-    demo_bucket = s3.Bucket('demo.sga.guru')
-    for obj in demo_bucket.objects.all():
-        print(obj.key)
-    # demo_bucket.download_file('index.html', '/tmp/index.html')
-    
-    build_bucket = s3.Bucket('build.demo.sga.guru')
-    #build_bucket.download_file('buildDemo.zip', '/tmp/buildDemo.zip')
-    
-    demo_zip = io.BytesIO()
-    build_bucket.download_fileobj('buildDemo.zip', demo_zip)
-    
-    # with zipfile.ZipFile(demo_zip) as myzip:
-    #     for nm in myzip.namelist():
-    #         print(nm)
-    with zipfile.ZipFile(demo_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            demo_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            #demo_bucket.Object(nm).Acl().put(ACL='public-read')
+    try:
+        demo_bucket = s3.Bucket('demo.sga.guru')
+        for obj in demo_bucket.objects.all():
+            print(obj.key)
+        # demo_bucket.download_file('index.html', '/tmp/index.html')
+        
+        build_bucket = s3.Bucket('build.demo.sga.guru')
+        #build_bucket.download_file('buildDemo.zip', '/tmp/buildDemo.zip')
+        
+        demo_zip = io.BytesIO()
+        build_bucket.download_fileobj('buildDemo.zip', demo_zip)
+        
+        # with zipfile.ZipFile(demo_zip) as myzip:
+        #     for nm in myzip.namelist():
+        #         print(nm)
+        with zipfile.ZipFile(demo_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                demo_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                #demo_bucket.Object(nm).Acl().put(ACL='public-read')
+        
+        topic.publish(Subject="Demo code deployed", Message="Deployed to bucket demo.sga.guru")
+        print("Function Completed - Bucket Updated")
+    except:
+        print("Function Failed")
+        topic.publish(Subject="Demo code deployment Failed", Message="Failed deployment to bucket demo.sga.guru")
+        raise
     
     return 'Hello from Lambda'
